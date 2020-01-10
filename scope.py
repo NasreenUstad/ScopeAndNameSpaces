577ab1331eb660dfd70627ae28fb8a78b19b6c62
nasreenustad <ustad.nasreen@gmail.com>
2020-01-10 18:46:33 +0530

    Scopes-Recursion-NameSpace

---
+++ scope.py
@@ -0,0 +1,31 @@
+def fact(n):
+    """ Calculate n! iteratively"""
+    result = 1
+    if n > 1:
+        for fact in range(2, n + 1):
+            print(f"{result} * {fact}")
+            result *= fact
+
+    return result
+
+
+def factorial(n):
+    """ Calculate n! recursively"""
+    if n <= 1:
+        print(1)
+        return 1
+    else:
+        print(f"{n} * {n-1}")
+        return n * factorial(n - 1)
+
+
+print(fact(4))
+print("-" * 30)
+# factorial(5)
+print(factorial(4))
+
+# for i in range(6):
+#     print(i)
+#     factorial(i)
+#     # print(f"{i}, {factorial(i)}")
+
